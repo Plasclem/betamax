881e6afa57162cf3c9a452f3e184f1f2278bd22a
Clement Plas <clement.plas@telecom-bretagne.eu>
2016-11-30 16:59:56 +0100

    Added Unittest to showcase the behaviour of MockHTTPResponse with headers containing LATIN1 encoded chars

--- tests/integration/test_unicode.py
+++ tests/integration/test_unicode.py
@@ -1,5 +1,8 @@
 from betamax import Betamax
 from tests.integration.helper import IntegrationHelper
+from betamax.mock_response import MockHTTPResponse
+from requests.packages.urllib3._collections import HTTPHeaderDict
+import unittest
 
 
 class TestUnicode(IntegrationHelper):
@@ -11,3 +14,14 @@ class TestUnicode(IntegrationHelper):
         with Betamax(s).use_cassette('test_unicode') as beta:
             self.cassette_path = beta.current_cassette.cassette_path
             s.get(url)
+
+
+class TestMockUnicode(unittest.TestCase):
+    """
+    Unittest to showcase the behaviour of MockHTTPResponse with headers containing LATIN1 encoded characters,
+    especially in filenames.
+    """
+
+    def test_mock_response_with_unicode(self):
+        headers = HTTPHeaderDict({'Content-Disposition': 'attachment; filename="\xe9";'})
+        MockHTTPResponse(headers)
